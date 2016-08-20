eebb792c948a03e6eef2d667bab550c70d50810a
wang huayang <huayang.wang@vipl.ict.ac.cn>
2016-08-20 08:49:16 +0800

    fix all test

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello world!" << endl;
-	cout << "add this to test diff" << endl;
+	cout << "add this to test diff before git add & gti commit" <<endl;
 	return 0;
 }
 
